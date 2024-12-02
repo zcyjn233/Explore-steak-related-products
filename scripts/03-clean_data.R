faf7b4fa83d67fa37157d47f53986ab09101c9c2
zcyjn233 <won.wang@mail.utoronto.ca>
2024-12-02 15:42:44 -0500

    Update 03-clean_data.R

--- scripts/03-clean_data.R
+++ scripts/03-clean_data.R
@@ -1,44 +1,43 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
-# License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Purpose: Cleans the data
+# Author: Wei Wang
+# Date: 2 December 2024 
+# Contact: won.wang@mail.utoronto.ca
 
 #### Workspace setup ####
 library(tidyverse)
+library(arrow)
+library(janitor)
+library(lubridate)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+raw_data <- read_csv("data/01-raw_data/hammer-4-raw.csv")
+product_data <- read_csv("data/01-raw_data/hammer-4-product.csv")
 
-cleaned_data <-
-  raw_data |>
-  janitor::clean_names() |>
-  select(wing_width_mm, wing_length_mm, flying_time_sec_first_timer) |>
-  filter(wing_width_mm != "caw") |>
-  mutate(
-    flying_time_sec_first_timer = if_else(flying_time_sec_first_timer == "1,35",
-                                   "1.35",
-                                   flying_time_sec_first_timer)
-  ) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "490",
-                                 "49",
-                                 wing_width_mm)) |>
-  mutate(wing_width_mm = if_else(wing_width_mm == "6",
-                                 "60",
-                                 wing_width_mm)) |>
-  mutate(
-    wing_width_mm = as.numeric(wing_width_mm),
-    wing_length_mm = as.numeric(wing_length_mm),
-    flying_time_sec_first_timer = as.numeric(flying_time_sec_first_timer)
-  ) |>
-  rename(flying_time = flying_time_sec_first_timer,
-         width = wing_width_mm,
-         length = wing_length_mm
-         ) |> 
+joined_data <- raw_data %>%
+  inner_join(product_data, by = c("product_id" = "id")) %>%
+  select(nowtime, 
+         vendor, 
+         product_id, 
+         product_name, 
+         brand, 
+         current_price, 
+         old_price, 
+         units, 
+         price_per_unit)
+
+cleaned_data <- joined_data %>% 
+  filter(vendor %in% c("Walmart","Loblaws")) %>%
+  select(nowtime, vendor, current_price, old_price, product_name) %>% 
+  mutate(month = month(nowtime),
+         current_price = parse_number(current_price),
+         old_price = parse_number(old_price)) %>%
+  filter(str_detect(tolower(product_name), "steak")) %>%
+  select(-nowtime) %>%
   tidyr::drop_na()
 
 #### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
+write_parquet(x = cleaned_data,
+              sink = "data/02-analysis_data/analysis_data.parquet")
+
+write_csv(cleaned_data, "/Users/lucky/Desktop/involves-original-work/data/02-analysis_data/analysis_data.csv") 
