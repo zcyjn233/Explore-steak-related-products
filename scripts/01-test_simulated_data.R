0a7028b6c5b6695402b42e5c49c80895a8d4da2b
zcyjn233 <won.wang@mail.utoronto.ca>
2024-12-02 20:22:22 -0500

    Update 01-test_simulated_data.R

--- scripts/01-test_simulated_data.R
+++ scripts/01-test_simulated_data.R
@@ -9,26 +9,27 @@
 library(tidyverse)
 library(testthat)
 
+# Load the simulated dataset
 simulated_data <- read_csv("data/00-simulated_data/simulated_data.csv")
 
-# Test that the dataset has 1000 rows
-test_that("dataset has 1000 rows", {
+# Test that the dataset contains exactly 1000 rows
+test_that("Dataset contains 1000 rows", {
   expect_equal(nrow(simulated_data), 1000)
 })
 
-# Test that the 'vendor' column contains values only from the predefined list
+# Test that the 'vendor' column contains only predefined values
 test_that("'vendor' column contains only predefined values", {
   predefined_vendors <- c("Walmart", "Loblaws")
   expect_true(all(simulated_data$vendor %in% predefined_vendors))
 })
 
-# Test that the 'current_price' and 'old_price' columns are numeric types
-test_that("'current_price' and 'old_price' are numeric", {
+# Test that 'current_price' and 'old_price' columns are numeric
+test_that("'current_price' and 'old_price' columns are numeric", {
   expect_type(simulated_data$current_price, "double")
   expect_type(simulated_data$old_price, "double")
 })
 
-# Test that the 'month' column contains values from 1 to 12
-test_that("Variable 'month' has unique values 1 to 12", {
+# Test that the 'month' column contains unique values from 1 to 12
+test_that("'month' column contains values from 1 to 12", {
   expect_setequal(unique(simulated_data$month), 1:12)
 })
